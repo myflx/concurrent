0791a3493a150b423abf3525e2078b20556d4eae
luoshanglin <1039466810@qq.com>
2019-07-07 17:39:21 +0800

    lifecycle

--- c1-thread/src/main/java/com/myflx/concurrent/thread/MyTask.java
+++ c1-thread/src/main/java/com/myflx/concurrent/thread/MyTask.java
@@ -4,17 +4,19 @@ import java.util.concurrent.TimeUnit;
 
 public class MyTask implements Runnable {
     public void run() {
+        doRun();
+    }
+
+    public static void doRun() {
+        System.out.println("task is running....");
         try {
-            for(;;){
-                System.out.println("task is running....");
-                TimeUnit.SECONDS.sleep(1);
-            }
+            TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) {
         new Thread(new MyTask()).start();
+        new Thread(MyTask::doRun).start();
     }
 }
